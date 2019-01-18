0338f23ba199b74d6cfc3a9beb9f402cc5ec89f0
jesper <jesper.lahdevirta@fortnox.se>
2019-01-18 14:57:30 +0100

    Added ui module

---
+++ ui/README.md
@@ -0,0 +1,2 @@
+# setup
+To run the frontend app you need npm 6 or greater.  
---
+++ ui/webpack.config.js
@@ -0,0 +1,89 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const babelConfig = {
+	presets: [
+		'@babel/preset-react',
+		[
+			'@babel/preset-env',
+			{
+				'targets': {
+					'browsers': [
+						'last 4 versions',
+						'ie 11'
+					]
+				}
+			}
+		]
+	],
+	plugins: [
+		'@babel/plugin-proposal-class-properties'
+	]
+};
+
+module.exports = {
+	entry: {
+		main: ['@babel/polyfill', './src/index.js']
+	},
+	output: {
+		publicPath: '/',
+		path: path.resolve(__dirname, 'dist'),
+		filename: '[name].[hash].js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader',
+				options: babelConfig
+			},
+			{
+				test: /\.less$/,
+				use: [
+					{
+						loader: MiniCssExtractPlugin.loader,
+						options: {
+							publicPath: '../'
+						}
+					},
+					'css-loader',
+					'less-loader'
+				]
+			},
+			{
+				test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+				loader: 'url-loader',
+				options: {
+					limit: 1000,
+					minetype: 'application/font-woff'
+				}
+			},
+			{
+				test: /\.(ttf|eot|svg|jpg|png)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+				loader: 'file-loader'
+			}
+		]
+	},
+	devtool: 'source-map', // TODO: Check production flag -> false
+	plugins: [
+		new MiniCssExtractPlugin({
+			filename: 'style.css'
+		}),
+		new CleanWebpackPlugin('dist', {}),
+		// new ExtractTextPlugin({filename: 'style.css'}),
+		new HtmlWebpackPlugin({
+			inject: false,
+			hash: true,
+			template: './src/index.html',
+			filename: 'index.html',
+		})
+	],
+	devServer: {
+		publicPath: '/',
+		historyApiFallback: true
+	}
+};
