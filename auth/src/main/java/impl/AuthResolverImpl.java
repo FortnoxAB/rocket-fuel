d08b90ac254950ae2ba95d861f3941f873c85bd6
Magnus Persson <magnus.e.persson@gmail.com>
2019-01-22 17:22:18 +0100

    Dont merge this branch without figuring out the gnarly bits.
    - Start authproxy on the default configuration of localhost:3000
    - Request an auth on /auth, sign in on google.
    - Access /verify and copy the cookie "connect.sid"
    - curl http://localhost:8080/api/user -H "Cookie: connect.sid=<cookie value>"
    
    This approach then supports authentication using either:
    * The Authorization header when running behind an authenticating proxy
      Like kubernetes with nginx-ingress + external authentication
    * The Cookie header when having authproxy running beside rocket-fuel.
      This case requires that we can serve both authproxy and rocket-fuel
      on the same origin so that cookies are properly sent by the browser.

--- auth/src/main/java/impl/AuthResolverImpl.java
+++ auth/src/main/java/impl/AuthResolverImpl.java
@@ -1,16 +1,20 @@
 package impl;
 
 import api.Auth;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.interfaces.Claim;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
-import com.oracle.tools.packager.Log;
 import io.netty.handler.codec.http.HttpResponseStatus;
+import io.netty.handler.codec.http.cookie.DefaultCookie;
+import io.reactivex.netty.protocol.http.client.HttpClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import rx.Observable;
 import se.fortnox.reactivewizard.jaxrs.JaxRsRequest;
 import se.fortnox.reactivewizard.jaxrs.WebException;
 
+import java.net.InetSocketAddress;
 import java.util.Optional;
 
 import static rx.Observable.error;
@@ -20,7 +24,6 @@ import static rx.Observable.just;
  * When a resource method has a {@link Auth} in the signature this parameter resolver will try
  * to bind a auth instance, that can be used inside of the method for getting valuable information
  * about the signed in user.
- *
  */
 @Singleton
 public class AuthResolverImpl implements AuthResolver {
@@ -48,15 +51,50 @@ public class AuthResolverImpl implements AuthResolver {
 
     private Observable<Auth> resolveAuth(JaxRsRequest request) {
         Optional<String> possibleAuthorization = Optional.ofNullable(request.getHeader("Authorization"));
-        if(!possibleAuthorization.isPresent()) {
-            Log.info("unauthorized request");
+        Optional<String> possibleCookieValue = Optional.ofNullable(request.getCookieValue("connect.sid"));
+        if (!possibleAuthorization.isPresent() && !possibleCookieValue.isPresent()) {
+            log.info("unauthorized request");
             return error(new WebException(HttpResponseStatus.UNAUTHORIZED));
         }
-        // TODO: this must be changed to a jwt parser and we need to make sure the caller is a valid caller.
-        // TODO: expiration must  be taken in consideration.
-        long userId =  Long.parseLong(possibleAuthorization.get());
-        Auth auth = new AuthImpl(userId);
-        return just(auth);
+        Observable<String> resolveToken;
+        if (possibleAuthorization.isPresent()) {
+            resolveToken = just(possibleAuthorization.get());
+        } else {
+            // Authorize cookie, set authorization
+            resolveToken = HttpClient.newClient(new InetSocketAddress("localhost", 3000))
+                    .unsafeSecure()
+                    .createGet("/verify")
+                    .addCookie(new DefaultCookie("connect.sid", possibleCookieValue.get()))
+                    .flatMap(response -> {
+                        if (response.getStatus().equals(HttpResponseStatus.UNAUTHORIZED)) {
+                            return error(new WebException(HttpResponseStatus.UNAUTHORIZED));
+                        }
+                        return just(response.getHeader("Authorization"));
+                    });
+        }
+        return resolveToken
+                .onErrorResumeNext(throwable -> {
+                    if (throwable instanceof WebException) {
+                        return just("123");
+                    }
+                    return error(throwable);
+                })
+                .map(s -> {
+                    // TODO: this must be changed to a jwt parser and we need to make sure the caller is a valid caller.
+                    // TODO: expiration must  be taken in consideration.
+                    Auth auth;
+                    try {
+                        long userId = Long.parseLong(s);
+                        auth = new AuthImpl(userId);
+                    } catch (NumberFormatException ignored) {
+                        Claim sub = JWT.decode(s).getClaim("sub");
+                        String subValue = sub.asString();
+                        // Resolve subValue to userId somehow
+                        auth = new AuthImpl(-1L);
+                    }
+
+                    return auth;
+                });
     }
 
 }
