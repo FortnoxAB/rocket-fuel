45cf17a61905d6a4639872d180fb88ee0d1315f9
Jonas Fredin <jonas.fredin@fortnox.se>
2019-07-02 14:44:59 +0200

    removed pathparams

--- impl/src/main/java/slack/SlackResource.java
+++ impl/src/main/java/slack/SlackResource.java
@@ -4,14 +4,13 @@ import com.github.seratch.jslack.api.model.Message;
 import com.github.seratch.jslack.api.model.block.LayoutBlock;
 import rx.Observable;
 
-import javax.ws.rs.PathParam;
 import java.util.List;
 
 public interface SlackResource {
 
-    Observable<String> getUserEmail(@PathParam("userId") String userId);
+    Observable<String> getUserEmail(String userId);
 
-    Observable<String> getUserId(@PathParam("email") String email);
+    Observable<String> getUserId(String email);
 
     Observable<Void> postMessageToSlack(String channel, String message, String threadTs);
 
